f11c1ad263d6f7c78dedd8fa877d191a6618bfdb
Aditya I. Satrio <adityaisatrio404@gmail.com>
2024-12-12 19:38:26 +0700

    function addRow dibuat lebih pendek dan efisien

--- script latihan.js
+++ script latihan.js
@@ -29,32 +29,20 @@ function validateForm(a, b, c, d) {
 //Fungsi tambah row di tabel
 let n = 6
 function addRow() {
-    document.getElementById('data').innerHTML+=
-        '<tr id="row_'+n+'">' +
-            '<td id="id_'+n+'">'+
-                n+
-            '</td>' +
-            '<td id="npm_'+n+'">'+
-    
-            '</td>' +
-            '<td id="nama_'+n+'">'+
-                
-            '</td>' +
-            '<td id="jenisKelamin_'+n+'">'+
-                
-            '</td>' +
-            '<td id="email_'+n+'">'+
-                
-            '</td>' +
-            '<td class="no-print">'+
-                '<button class="edit-button" onclick="fillForm('+n+')">'+
-                    '<i class="bi bi-pencil"></i>Edit'+
-                '</button>'+
-                '<button class="delete-button" onclick="deleteRow('+n+')">'+
-                        '<i class="bi bi-trash3"></i>'+
-                    '</button>'+
-            '</td>'+
-        '</tr>';
+    const row = document.createElement('tr');
+    row.id = `row_${n}`;
+    row.innerHTML = `
+        <td id="id_${n}">${n}</td>
+        <td id="npm_${n}"></td>
+        <td id="nama_${n}"></td>
+        <td id="jenisKelamin_${n}"></td>
+        <td id="email_${n}"></td>
+        <td class="no-print">
+            <button class="edit-button" onclick="fillForm(${n})"><i class="bi bi-pencil"></i>Edit</button>
+            <button class="delete-button" onclick="deleteRow(${n})"><i class="bi bi-trash3"></i></button>
+        </td>
+    `;
+    document.getElementById('data').appendChild(row);
     n++;
 }
 //Menghapus row di tabel
